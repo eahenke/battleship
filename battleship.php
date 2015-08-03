ef0f5c4949a5a2327a54725d9ded580d077fae41
henkrs1 <eahenke@gmail.com>
2015-08-02 17:55:15 -0700

    made a little prettier

--- battleship.php
+++ battleship.php
@@ -13,103 +13,118 @@
 
 	<body>
 
-		<h1>BATTLESHIP</h1>
+		<!-- <h1>BATTLESHIP</h1> -->
 
-		<div class="turn"></div>
-
-		<div class="new">New Game</div>
+		<!-- To center title relative to boards, put in boardwrap, then change info-area margin-top -->
 
 		<div class="board-wrap">
-			<div class="board human-board">
-			<!-- <p>Player Board</p>
-			<p>AI Board</p> -->
-				
-				<?php
-				$rows = range('A', 'J');
-				$columns = range(1, 10);
-				?>
-
-				<div class="labels">
-					<div class="row-label">
-						<?php foreach($rows as $row) { ?>
-							<div class="tile label" id="<?php echo $row; ?>"><?php echo $row; ?></div>
-						<?php } ?>
-					</div>
-
-					<div class="column-label">
-						<?php foreach($columns as $col) { ?>
-							<div class="tile label" id="<?php echo $col; ?>"><?php echo $col; ?></div>
-						<?php } ?>
-					</div>
-				</div><!--Labels-->
-
-				<div class="game-area">
+			<h1>BATTLESHIP</h1>
+			<div class="single-board-wrap">
+				<h2>Human Board</h2>
+				<div class="board human-board">
+				<!-- <p>Player Board</p>
+				<p>AI Board</p> -->
 					
-					<?php /* foreach($rows as $row) {
-						$count = 0;
+					<?php
+					$rows = range('A', 'J');
+					$columns = range(1, 10);
+					?>
 
-						foreach($columns as $col) {
-							$coord = $row . '-' . $col;
-							if($count == 0) { ?>
-								<div class="clear tile" id="<?php echo $coord;?>"></div>	
-							 <?php } else { ?>
-							 	<div class="tile" id="<?php echo $coord;?>"></div>	
-							 <?php }
-							 $count++;
-						} //end cols 
-					} //end rows */ ?>
-		
+					<div class="labels">
+						<div class="row-label">
+							<?php foreach($rows as $row) { ?>
+								<div class="tile label" id="<?php echo $row; ?>"><?php echo $row; ?></div>
+							<?php } ?>
+						</div>
+
+						<div class="column-label">
+							<?php foreach($columns as $col) { ?>
+								<div class="tile label" id="<?php echo $col; ?>"><?php echo $col; ?></div>
+							<?php } ?>
+						</div>
+					</div><!--Labels-->
+
+					<div class="game-area">
+						
+						<?php /* foreach($rows as $row) {
+							$count = 0;
+
+							foreach($columns as $col) {
+								$coord = $row . '-' . $col;
+								if($count == 0) { ?>
+									<div class="clear tile" id="<?php echo $coord;?>"></div>	
+								 <?php } else { ?>
+								 	<div class="tile" id="<?php echo $coord;?>"></div>	
+								 <?php }
+								 $count++;
+							} //end cols 
+						} //end rows */ ?>
+			
+					</div>
 				</div>
-			</div>
+			</div> <!--single board wrapper-->
 
-			<div class="board ai-board">
-				
-				<?php
-				$rows = range('A', 'J');
-				$columns = range(1, 10);
-				?>
-
-				<div class="labels">
-					<div class="row-label">
-						<?php foreach($rows as $row) { ?>
-							<div class="tile label" id="<?php echo $row; ?>"><?php echo $row; ?></div>
-						<?php } ?>
-					</div>
-
-					<div class="column-label">
-						<?php foreach($columns as $col) { ?>
-							<div class="tile label" id="<?php echo $col; ?>"><?php echo $col; ?></div>
-						<?php } ?>
-					</div>
-				</div><!--Labels-->
-
-				<div class="game-area">
+			<div class="single-board-wrap">
+				<h2>AI Board</h2>
+				<div class="board ai-board">
 					
-					<?php /* foreach($rows as $row) {
-						$count = 0;
+					<?php
+					$rows = range('A', 'J');
+					$columns = range(1, 10);
+					?>
 
-						foreach($columns as $col) {
-							$coord = $row . '-' . $col;
-							if($count == 0) { ?>
-								<div class="clear tile" id="<?php echo $coord;?>"></div>	
-							 <?php } else { ?>
-							 	<div class="tile" id="<?php echo $coord;?>"></div>	
-							 <?php }
-							 $count++;
-						} //end cols 
-					} //end rows */ ?>
-		
+					<div class="labels">
+						<div class="row-label">
+							<?php foreach($rows as $row) { ?>
+								<div class="tile label" id="<?php echo $row; ?>"><?php echo $row; ?></div>
+							<?php } ?>
+						</div>
+
+						<div class="column-label">
+							<?php foreach($columns as $col) { ?>
+								<div class="tile label" id="<?php echo $col; ?>"><?php echo $col; ?></div>
+							<?php } ?>
+						</div>
+					</div><!--Labels-->
+
+					<div class="game-area">
+						
+						<?php /* foreach($rows as $row) {
+							$count = 0;
+
+							foreach($columns as $col) {
+								$coord = $row . '-' . $col;
+								if($count == 0) { ?>
+									<div class="clear tile" id="<?php echo $coord;?>"></div>	
+								 <?php } else { ?>
+								 	<div class="tile" id="<?php echo $coord;?>"></div>	
+								 <?php }
+								 $count++;
+							} //end cols 
+						} //end rows */ ?>
+			
+					</div>
 				</div>
-			</div>
+			</div><!--single board wrapper-->
 		</div>
 
-		<div class="scoreboard">
-			<p>SCOREBOARD</p>
-			<div class="ships-left">
-				<p>Enemy ships remaining:</p>
+		<div class="info-area">
+			<h2>SCOREBOARD</h2>
+			<div class="scoreboard">
+				<div class="ships-left">
+					<p>Enemy ships remaining:</p>
+				</div>
 			</div>
 			
+			<div class="gamelog-container">
+				<h2>GAME LOG</h2>
+			</div>	
+
+
 		</div>
+		
+		<div class="clear"></div>
+		<div class="new">New Game</div>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script src="functions.js"></script>
